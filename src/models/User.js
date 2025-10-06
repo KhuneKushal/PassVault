671fe36c502592d0ca588537fca9e1d3117c4d31
KhuneKushal <kushalkhune111@gmail.com>
2025-10-06 12:46:24 -0700

    fix: Remove double password hashing in User model
    Removes the pre-save middleware that was hashing already-hashed passwords, which prevented user login.

--- src/models/User.js
+++ src/models/User.js
@@ -19,14 +19,6 @@ const userSchema = new mongoose.Schema({
   }
 });
 
-// Hash password before saving
-userSchema.pre('save', async function(next) {
-  if (this.isModified('hashedPassword')) {
-    this.hashedPassword = await bcrypt.hash(this.hashedPassword, 10);
-  }
-  next();
-});
-
 // Add method to compare password
 userSchema.methods.comparePassword = async function(candidatePassword) {
   return bcrypt.compare(candidatePassword, this.hashedPassword);
